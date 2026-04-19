1b7e3093edbb470ea1a9e9e4a113bcb4dffc8a82
yukiipaw <cookiesuwu25@gmail.com>
2026-04-20 00:07:30 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -30,9 +30,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "eating snow..",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 4, 
       },
     ],
   },
